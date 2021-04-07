bb1da3f14f346aa10ce589e77d18ac5a2476509f
CwsCby <790927114@qq.com>
2021-04-07 17:19:47 +0800

    修改1.js

--- 1.js
+++ 1.js
@@ -4,6 +4,7 @@
  * @Author: xin
  * @Date: 2021-04-07 17:09:56
  * @LastEditors: xin
- * @LastEditTime: 2021-04-07 17:10:17
+ * @LastEditTime: 2021-04-07 17:19:34
  */
-console.log(123)
+console.log(123);
+console.log(456, '--gitTest2');
