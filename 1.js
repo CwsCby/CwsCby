be293551abe2876132ff1bea97b57d740185f6dc
CwsCby <790927114@qq.com>
2021-04-07 17:22:24 +0800

    修改1.js2

--- 1.js
+++ 1.js
@@ -4,6 +4,6 @@
  * @Author: xin
  * @Date: 2021-04-07 17:09:56
  * @LastEditors: xin
- * @LastEditTime: 2021-04-07 17:10:17
+ * @LastEditTime: 2021-04-07 17:19:08
  */
-console.log(123)
+console.log(234);
